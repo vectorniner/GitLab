7f5285d4ce1cd7f64fc94940cc82a2b60566a52a
Sirtobey24 <37386453+Sirtobey24@users.noreply.github.com>
2018-04-02 14:23:08 -0700

    Update rpgGame.c

--- rpgGame.c
+++ rpgGame.c
@@ -517,13 +517,38 @@ void main(void)
 					}
 					break;
 			}
-			case 15:
+			case 15://Sean
 			{
-					while(choice != 99)
+									 int userinput;
+					printf("Hello! Welcome, gamer! You are now in room 15...");
+					printf("You hear a loud growl. Suddenly you hear loud footsteps. You panick but you try to rely on your
+					instincts");
+					printf("You are shocked to your eyes to see a massive bear coming out of a cave!");
+					printf("You have a piece of rock laying next to you and a long club laying further away");
+					printf("What do you do? Please choose from the following options!");
+					printf("Press 99 if you want to quit since video games cause violence");
+
+					scanf(%d, &userinput);
+					while(userinput!=99)
 					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
+						if(userinput ==1)
+						{
+							printf("You automatically rely on running out of the room. The room is locked out and there is no other exit besides the
+								cave that the bear came out of. Since you're panick attack has gotten worse, you make poor judgments and gets
+								beaten to death by the bear. Now you have lost your legacy!");
+						}
+						else if(userinput ==2)
+						{
+							printf("You decide to make friends with the bear. It turns out that the bear does not speak English, therefore, you instantly get eaten by the bear!");
+
+						}
+						else if(userinput==3)
+						{
+							printf("You decide to use the stone rod that is laying on the floor. You end up getting bruised, although you emerge victorious after beating the bear to death and you feel accomplished!");
+						}
 					}
+
+        
 					break;
 			}
 			case 16:
