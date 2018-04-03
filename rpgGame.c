75683b1034dab9c0f5148a9d2178f0082c738712
Kevin Maatubang <kmaatubang1@nsmb208-xx.csudh.edu>
2018-04-02 17:11:43 -0700

    Room 17

--- rpgGame.c
+++ rpgGame.c
@@ -1,6 +1,6 @@
 //Contributors
 //Garrett Poppe 3/19/18
-//
+//Kevin Maatubang 4/2/18
 //
 
 #include <stdio.h>
@@ -172,15 +172,54 @@ void main(void)
 					}
 					break;
 			}
-			case 17:
+			case 17: // This room just adds to the game. Can be edited later to result in a loss and terminate the program. Currently, solving or failing to solve the problem ends up with the player choosing a room again
 			{
-					while(choice != 99)
+				while(choice != 99)
+				{
+					puts("\nYou open the door to room 17.");
+					puts("Upon entering, the door slams behind you and you hear it lock.");
+					puts("A lightbulb hanging from the ceiling begins to light up. After waiting a few moments, the lights slightly turn up and you notice something in the center of the room.");
+					puts("In the center of the room, you see a small table with a number pad embedded onto it.");
+				        puts("Upon inspection of the number pad, you press it  and the whole room suddenly lights up entirely, putting you in a daze.");
+					puts("You recover from the daze. Upon recovering, you see a small screen display hanging in front of you connected to the number pad.");
+					puts("Even worse, you notice multiple explosives, enough to blow the room (and yourself) out of existence.");
+					puts("On the screen display, it shows: Enter the correct 4 number password. After 3 wrong attempts, say bye to your life.");
+					puts("You look around the room and see a lot of scratch and scribbles, but you notice something barely legible.");
+					puts("You decipher it and it says roomx365."); // The 4 number password is 17 * 365, which is 6205
+					puts("You question the meaning of it as you hesitate to approach the number pad.");
+					puts("You must now enter the 4 number password.");
+
+					int i;																										   for(i = 1; i<4; i++)
 					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
+						scanf("%d", &choice);
+						if(choice != 6205)
+						{
+							printf("\nYou guessed wrong. Attempt %d failed.\n", i);
+							if(i ==3)
+							{
+								puts("\nAll attempts failed, goodbye.");
+								puts("The explosion magically blows you back to where you started. Hey at least you aren't dead ... yet\n");
+								//choice = 99; //uncommenting would execute this: puts("Magically you are transported through space and time. You are back at the beginning....."); and end the program	break;
+							}
+						}																																					
+						else if(choice == 6205)
+						{
+							puts("\nThe screen displays: Congratulations, you can math, as you hear the door unlock and open behind you.");
+							puts("You never sprinted so fast in your life as you exit the room.");
+							puts("As you are about to exit the room, a portal appears out of no where and you inadvertently run through it...\n"); 
+							// After the correct choice, the player is transported back to the beginning as if nothing happened
+							break;
+						}
+						else
+						{
+							printf("You guessed wrong. Attempt %d failed.", i);
+							break;
+						}	
+					}			
+					break; // Break statement needed here to avoid infinite loop of room 17 whether correct or wrong	
+				}				
+				break;	
+			}		
 			case 18:
 			{
 					while(choice != 99)
