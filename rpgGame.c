f8333c3f0f582660d1684ebb2b07e3a17526684c
Sirtobey24 <37386453+Sirtobey24@users.noreply.github.com>
2018-04-09 15:17:12 -0700

    Update rpgGame.c

--- rpgGame.c
+++ rpgGame.c
@@ -519,36 +519,56 @@ void main(void)
 			}
 			case 15://Sean
 			{
-									 int userinput;
-					printf("Hello! Welcome, gamer! You are now in room 15...");
-					printf("You hear a loud growl. Suddenly you hear loud footsteps. You panick but you try to rely on your
-					instincts");
-					printf("You are shocked to your eyes to see a massive bear coming out of a cave!");
-					printf("You have a piece of rock laying next to you and a long club laying further away");
-					printf("What do you do? Please choose from the following options!");
-					printf("Press 99 if you want to quit since video games cause violence");
-
-					scanf(%d, &userinput);
-					while(userinput!=99)
+					
+					while(choice !=99)
 					{
-						if(userinput ==1)
+						
+
+							printf("Hello! Welcome, gamer! You are now in room 15...");
+							printf("You hear a loud growl. Suddenly you hear loud footsteps. You panick but you try to rely on your instincts");
+							printf("You are shocked to your eyes to see a massive bear coming out of a cave!");
+							printf("You have a piece of rock laying next to you and a long club laying further away");
+							printf("What do you do? Please choose from the following options!");
+							printf("Press 99 if you want to quit since video games cause violence");
+
+					scanf(%d, &choice);
+						int roll_0 =(rand() %10) +1;
+						int roll1 = (rand() %10) + 1;
+						int die = 0;
+
+
+						if(choice ==1)
 						{
-							printf("You automatically rely on running out of the room. The room is locked out and there is no other exit besides the
-								cave that the bear came out of. Since you're panick attack has gotten worse, you make poor judgments and gets
-								beaten to death by the bear. Now you have lost your legacy!");
+							printf("You automatically rely on running out of the room. The room is locked out and there is no other exit besides the 
+										cave that the bear came out of. Since you're panick attack has gotten worse, you make poor judgments and gets
+							beaten to death by the bear. Now you have lost your legacy!");
 						}
-						else if(userinput ==2)
+						else if(choice ==2)
 						{
-							printf("You decide to make friends with the bear. It turns out that the bear does not speak English, therefore, you instantly get eaten by the bear!");
+							printf("You decide to make friends with the bear. The bear seems to utter some sounds. It appears to speak a human language, but you are just being delusional. It turns out that the bear does not speak English, therefore, you instantly get eaten by the bear!");
 
 						}
-						else if(userinput==3)
+						else if(choice==3)
 						{
-							printf("You decide to use the stone rod that is laying on the floor. You end up getting bruised, although you emerge victorious after beating the bear to death and you feel accomplished!");
+							printf("You decide to use the stone rod that is laying on the floor. You end up getting bruised, although you emerge victorious after beating the bear to death and you feel accomplished!. Now you can move on to other rooms. Good luck on your journey!");
+									for (i = 0; i < die; i++);
+									{
+										if(roll_0 == roll1)
+										{
+											printf("You won the jackpot! Nobody knows what it is however...");
+										}
+									}
+									FILE *outfile = fopen( "outfile.txt", "a");
+									fclose(outfile);
+
+								}
+
+						else
+						{
+							printf("Please enter a valid option or just play a different game you rebel...");
 						}
+       
 					}
-
-        
 					break;
 			}
 			case 16:
